8e56fb9e69cabcd720c533c0183687db23b0f21c
Xavier Hazzard <xavier@hazzard.org.uk>
2021-03-25 18:21:37 +0000

    Finished main body

--- app.js
+++ app.js
@@ -24,14 +24,41 @@ btn.addEventListener("click", () => {
     //         "hidden";
     // }, 1200);
 
-    sleep(2000).then(() => {
+    sleep(1000).then(() => {
         load.classList.remove(
             "fade-in"
         );
         load.classList.add("fade-out");
-        load.style.visibility =
-            "hidden";
+        sleep(500).then(() => {
+            load.classList.remove(
+                "fade-out"
+            );
+            load.style.visibility =
+                "hidden";
+        });
     });
+
+    // fetch("http://127.0.0.1:3000/api", {
+    //     method: "GET",
+    //     mode: "no-cors",
+    // })
+    //     .then((res) => {
+    //         return res.json();
+    //     })
+    //     .then((res) => {
+    //         console.log(res);
+    //     });
+    fetch("http://127.0.0.1:3000/api")
+        .then((res) => {
+            // console.log(JSON.parse(res));
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 });
 
 // function sleep(time) {
@@ -41,6 +68,26 @@ btn.addEventListener("click", () => {
 //         }, time);
 //     });
 // }
+
+// async function fetchIdea() {
+//     const res = await fetch(
+//         "http://172.17.150.255:3000/api",
+//         {
+//             method: "GET",
+//             mode: "no-cors",
+//             headers: {
+//                 "Content-Type":
+//                     "application/json",
+//             },
+//         }
+//     );
+//     const resSon = await res.json();
+//     return resSon;
+// }
+
+// fetchIdea().then((ideas) => {
+//     console.log(ideas);
+// });
 
 function sleep(ms) {
     // add ms millisecond timeout before promise resolution
