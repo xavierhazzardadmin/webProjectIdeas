e3e0ccc0b389f56fdfe27eb06aaa4cc0cc3680f6
Xavier Hazzard <xavier@hazzard.org.uk>
2021-03-26 11:21:29 +0000

    Removed all everything not needed

--- app.js
+++ app.js
@@ -17,15 +17,6 @@ let num;
 btn.addEventListener("click", () => {
     load.style.visibility = "visible";
     load.classList.add("fade-in");
-    // setTimeout(() => {
-    //     load.classList.remove(
-    //         "fade-in"
-    //     );
-    //     load.classList.add("fade-out");
-    //     load.style.visibility =
-    //         "hidden";
-    // }, 1200);
-    // sleep(400).then(fetchIdea());
 
     setTimeout(() => {
         fetchIdea();
@@ -44,46 +35,7 @@ btn.addEventListener("click", () => {
                 "hidden";
         });
     });
-
-    // fetch("http://127.0.0.1:3000/api", {
-    //     method: "GET",
-    //     mode: "no-cors",
-    // })
-    //     .then((res) => {
-    //         return res.json();
-    //     })
-    //     .then((res) => {
-    //         console.log(res);
-    //     });
 });
-
-// function sleep(time) {
-//     return new Promise(resolve, () => {
-//         setTimeout(() => {
-//             resolve();
-//         }, time);
-//     });
-// }
-
-// async function fetchIdea() {
-//     const res = await fetch(
-//         "http://172.17.150.255:3000/api",
-//         {
-//             method: "GET",
-//             mode: "no-cors",
-//             headers: {
-//                 "Content-Type":
-//                     "application/json",
-//             },
-//         }
-//     );
-//     const resSon = await res.json();
-//     return resSon;
-// }
-
-// fetchIdea().then((ideas) => {
-//     console.log(ideas);
-// });
 
 function sleep(ms) {
     // add ms millisecond timeout before promise resolution
@@ -109,14 +61,10 @@ function fetchIdea() {
         "https://powerful-basin-38985.herokuapp.com/api"
     )
         .then((res) => {
-            // console.log(JSON.parse(res));
             return res.json();
         })
         .then((data) => {
             const len = data.length;
-            // const rand = Math.floor(
-            //     Math.random() * len
-            // );
             console.log(
                 data[random(data)]
             );
